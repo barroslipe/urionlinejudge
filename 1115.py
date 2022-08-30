7d01e879507af6540e6b0ce5138b9850491bb9f1
Felipe Barros de Paula Leite <barroslipe@gmail.com>
2022-08-29 21:43:41 -0300

    Problema resolvido

---
+++ 1115.py
@@ -0,0 +1,15 @@
+x, y = map(int, input().split())
+
+while x != 0 and y != 0:
+    if x > 0:
+        if y > 0:
+            print('primeiro')
+        else:
+            print('quarto')
+    else:
+        if y > 0:
+            print('segundo')
+        else:
+            print('terceiro')
+
+    x, y = map(int, input().split())
